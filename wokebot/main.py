69969ce5b3732b640fedb41f6e6115cdb7c8fd47
wokexed <ray.lee@ventitechnologies.com>
2024-07-05 15:51:35 +0800

    birth of wokebot

---
+++ wokebot/main.py
@@ -0,0 +1,30 @@
+import os
+
+from dotenv import load_dotenv
+from discord import Intents, Client, Message
+
+import discord
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = 1258684017943777330
+
+intents = discord.Intents.default()
+intents.typing = False
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    channel = client.get_channel(CHANNEL_ID)
+    if channel:
+        await channel.send("All systems online")
+    else:
+        print("Failed to find channel :<")
+    print(f'{client.user} has connected to the server!')
+
+if TOKEN:
+    client.run(TOKEN)
+else:
+    print("ERROR: Discord token not found in environment variables.")
